8293d87e3c1d22fad494977fa61e38e7731e0ded
S-Vortex <s@s-vortex.com>
2015-02-18 20:52:22 -0600

    Changed main menu

--- Scripture Alarm Prototype/Scripture Alarm Prototype/Form1.cs
+++ Scripture Alarm Prototype/Scripture Alarm Prototype/Form1.cs
@@ -29,5 +29,20 @@ namespace Scripture_Alarm_Prototype
             texta.
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
